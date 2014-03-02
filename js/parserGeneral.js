135f28248af4e0f9c4ad8756d64e7b9eac2c6f3c
Cheng Gong <cheng@G.c>
2014-03-02 11:37:17 -0500

    parserGeneral, generate a json object of size 45

--- js/parserGeneral.js
+++ js/parserGeneral.js
@@ -1,7 +1,8 @@
 //url to parse: http://www.healthycanadians.gc.ca/recall-alert-rappel-avis/search-recherche/simple?s=&plain_text=&f_mc=1&js_en=&page=1000
-var resultTxt = '{ "items" : [';
+var parsed = '';
 function parseBriefInfo() {
   var temp = '';
+  var resultTxt = '{ "items":[';
   var contents = document.getElementsByClassName('single_awr');
   for(i = 0; i<contents.length; i++){
     var current = contents[i];
@@ -11,10 +12,22 @@ function parseBriefInfo() {
     title = title.replace(/\s{2,}/g, ' ');
     url = url.replace(/\s{2,}/g, ' ');
     description = description.replace(/\s{2,}/g, ' '); 
-    temp+= '{ "itemName":' + '"' + title + '",' + '"description":' + '"' + description + '",' + '"url:" "' + url + '"},' + "\n"; 
+    temp+= '{ "itemName":' + '"' + title + '",\n' + '"description":' + '"' + description + '",' + '"url": "' + url + '"},' + "\n"; 
   }
-  temp = temp.substring(0, temp.length - 3);
-  resultTxt = temp + ']}';
-  console.log(resultTxt);
+  temp = temp.substring(0, temp.length - 2);
+  resultTxt += temp + ']}';
+  //resultTxt = JSON.stringify(eval("(" + resultTxt + ")"));
+  //console.log(resultTxt);
+  //resultTxt = jQuery.parseJSON( resultTxt );
+  parsed = jQuery.parseJSON( resultTxt );
+}
+
+
+{"items":[
+  {
+    "itemName":" Undeclared milk in Burnbrae Farms brand Dried Whole Eggs ",
+    "description":" Undeclared milk in Burnbrae Farms brand Dried Whole Eggs Dried Whole Eggs 22.7 kg Burnbrae Farms food recall, food recalls, allergy alert, allergy alerts, public warning, food safety, food borne illness, food poisoning, food allergy, food allergies, milk, Burnbrae Farms... ",
+    "url":"http://www.healthycanadians.gc.ca/recall-alert-rappel-avis/inspection/2014/38163r-eng.php"}
+  ]
 }
 
